c59fdfb5b98733d3b2da4455aaefaead1391faf6
Ngurah27 <ngurahpratama647@gmail.com>
2025-10-03 19:24:12 +0800

    Delete cek_koneksi.php

--- cek_koneksi.php
+++
@@ -1,8 +0,0 @@
-<?php
-include "proses/connect.php";
-
-$res = mysqli_query($conn, "SELECT DATABASE() AS db");
-$row = mysqli_fetch_assoc($res);
-
-echo "DEBUG: Saat ini terkoneksi ke database = " . $row['db'];
-?>
